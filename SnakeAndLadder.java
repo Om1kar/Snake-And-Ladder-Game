1eb9204cd6e1829161e6b7d303cecf15a1ee40f6
omkar <omkardudhewar121@gmail.com>
2022-11-23 09:19:17 +0530

    added welcome message to user

---
+++ SnakeAndLadder.java
@@ -0,0 +1,15 @@
+package SnakeAndLadder;
+
+/**
+ *Snake Ladder Game By Java
+ */
+public class SnakeAndLadder {
+    /**
+     *
+     * @param -welcome message to user
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome To Snake And Ladder Game");
+    }
+
+}
