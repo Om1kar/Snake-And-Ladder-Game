1998715c0b2ef9984da0c475a443be6c5dce0abc
omkar <omkardudhewar121@gmail.com>
2022-11-23 12:36:54 +0530

    player reaches exact 100th position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,52 +5,62 @@ package SnakeAndLadder;
  */
 public class SnakeAndLadder {
     /**
-     * @param -UC4-Repeat till the Player
-     * reaches the winning position 100.
-     *
+     * @param -UC5-Ensure the player gets to exact winning position 100.
      */
     public static final int NO_PLAY = 1;
-    public static final int Snake = 2;
-    public static final int Ladder = 3;
+    public static final int SNAKE = 2;
+    public static final int LADDER = 3;
 
     public static void main(String[] args) {
         System.out.println("-----------------------------------");
         System.out.println("Welcome To Snake And Ladder Game");
         System.out.println("------------------------------------");
+
         int playerPosition = 0;
         System.out.println("player at Position =" + playerPosition);
-        while (playerPosition <=100) {
+
+        while (playerPosition != 100) {
+
             int rollDice = (int) Math.floor(Math.random() * 10) % 6 + 1;
-            System.out.println("After Dice Rolls players Position =" + rollDice);
+            System.out.println("After Dice Rolls player gets =" + rollDice);
 
             int options = (int) Math.floor(Math.random() * 10) % 3 + 1;
             System.out.println("player checks for options =" + options);
 
             switch (options) {
                 case NO_PLAY:
-                    System.out.println("players position =" + rollDice);
+                    System.out.println("players position =" + playerPosition);
+                    System.out.println("-----------------------------------------------");
                     break;
-                case Snake:
+                case SNAKE:
                     playerPosition = playerPosition - rollDice;
-                    System.out.println("Player Got Snake and need to go back by " + playerPosition + " Steps");
+                    if (playerPosition >= 0) {
+                        System.out.println("Players Current Position= " + playerPosition);
+                        System.out.println("-----------------------------------------------");
+
+                    } else {
+                        playerPosition = 0;
+                        System.out.println("players Current Position =" + playerPosition);
+                        System.out.println("-------------------------------------------------");
+                    }
                     break;
-                case Ladder:
+                case LADDER:
                     playerPosition = playerPosition + (rollDice * 2);
                     System.out.println("Player got Ladder");
-                    System.out.println("New position after getting Ladder = " + playerPosition);
+                    System.out.println("Player's Position " + playerPosition);
+                    System.out.println("---------------------------------------------------------");
                     break;
+
                 default:
                     System.out.println("Player Wants to Quit The Game");
             }
         }
-        if (playerPosition>=100){
+        if (playerPosition >= 100) {
             System.out.println("======================================================");
             System.out.println("                PLAYER WON THE GAME                   ");
             System.out.println("======================================================");
         }
-        else {
-            System.out.println("Roll Die Again");
-        }
     }
 
 }
+
