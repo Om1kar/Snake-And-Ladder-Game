9fe45a3aed20ae2b40739231dc18a5584f60a740
omkar <omkardudhewar121@gmail.com>
2022-11-23 09:33:03 +0530

    player at starting position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,15 +1,17 @@
 package SnakeAndLadder;
 
 /**
- *Snake Ladder Game By Java
+ * Snake Ladder Game By Java
  */
 public class SnakeAndLadder {
     /**
-     *
-     * @param -welcome message to user
+     * @param -UC1 Snake and Ladder game played with single player at start
+     *             position 0
      */
     public static void main(String[] args) {
         System.out.println("Welcome To Snake And Ladder Game");
+        int playerPosition = 0;
+        System.out.println("player at Position=" + playerPosition);
     }
 
 }
